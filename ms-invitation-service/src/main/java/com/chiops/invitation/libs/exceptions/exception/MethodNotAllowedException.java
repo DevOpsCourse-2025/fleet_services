f1684cfb37333d290adbc75de6190f8fb043226c
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 22:06:14 -0600

    Delete ms-invitation-service directory

--- ms-invitation-service/src/main/java/com/chiops/invitation/libs/exceptions/exception/MethodNotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.invitation.libs.exceptions.exception;
-
-public class MethodNotAllowedException extends RuntimeException {
-    public MethodNotAllowedException(String message) {
-        super(message);
-    }
-}
--- ms-invitation-service/src/main/java/com/chiops/invitation/libs/exceptions/exception/PreconditionFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.invitation.libs.exceptions.exception;
-
-public class PreconditionFailedException extends RuntimeException {
-    public PreconditionFailedException(String message) {
-        super(message);
-    }
-}
