bd5b1434eec115b6a4019b7f241b479a6945b0d4
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 22:56:00 -0600

    Delete ms-invitation-service directory

--- ms-invitation-service/src/main/java/com/chiops/invitation/libs/exceptions/exception/ParseError.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.invitation.libs.exceptions.exception;
-
-public class ParseError extends BadRequestException {
-    public ParseError(String message) {
-        super(message);
-    }
-}
