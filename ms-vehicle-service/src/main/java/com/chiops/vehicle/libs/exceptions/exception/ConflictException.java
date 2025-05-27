0a0a41a48dbc57972c2ced700e12858dd19677c9
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 22:03:40 -0600

    Delete ms-vehicle-service directory

--- ms-vehicle-service/src/main/java/com/chiops/vehicle/libs/exceptions/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.vehicle.libs.exceptions.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}
