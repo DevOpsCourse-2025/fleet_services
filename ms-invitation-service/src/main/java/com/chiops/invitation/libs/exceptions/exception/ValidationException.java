8f07bca4d395255416e3f835dc02d6838fa73267
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 21:19:35 -0600

    Delete ms-invitation-service directory

--- ms-invitation-service/src/main/java/com/chiops/invitation/libs/exceptions/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.invitation.libs.exceptions.exception;
-
-public class ValidationException extends BadRequestException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
