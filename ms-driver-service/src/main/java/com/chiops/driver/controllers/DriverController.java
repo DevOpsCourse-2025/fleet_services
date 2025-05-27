9a156d9f03a7824f27704fb4ec32a1a48dd66051
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 21:22:38 -0600

    Update DriverController.java

--- ms-driver-service/src/main/java/com/chiops/driver/controllers/DriverController.java
+++ ms-driver-service/src/main/java/com/chiops/driver/controllers/DriverController.java
@@ -29,6 +29,18 @@ public class DriverController {
             throw new InternalServerException("Internal server error while trying to create the driver: " + e.getMessage());
         }
     }
+    
+    @Put("/update")
+    public DriverDTO updateDriver(@Body DriverDTO driverDTO) {
+        try {
+            return driverService.updateDriver(driverDTO);
+        } catch (BadRequestException e) {
+            throw new BadRequestException("Bad request while trying to update the driver: " + e.getMessage());
+        } catch (InternalServerException e) {
+            throw new InternalServerException("Internal server error while trying to update the driver: " + e.getMessage());
+        }
+    }
+
 
     @Get("/get/{curp}")
     public DriverDTO getDriverByCurp(@PathVariable String curp) {
