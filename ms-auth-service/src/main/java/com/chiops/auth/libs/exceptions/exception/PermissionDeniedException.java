d6f047c35be75ac86bd543761811ae61bab05ee9
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 22:49:01 -0600

    Delete ms-auth-service directory

--- ms-auth-service/src/main/java/com/chiops/auth/libs/exceptions/exception/PermissionDeniedException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.auth.libs.exceptions.exception;
-
-public class PermissionDeniedException extends RuntimeException {
-    public PermissionDeniedException(String message) {
-        super(message);
-    }
-}
