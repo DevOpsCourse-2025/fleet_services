945a0c4006892f4d2b71c3def2aff8b79ce836d8
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 22:40:31 -0600

    Delete ms-invitation-service directory

--- ms-invitation-service/src/main/java/com/chiops/invitation/libs/exceptions/exception/PermissionDeniedException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.invitation.libs.exceptions.exception;
-
-public class PermissionDeniedException extends RuntimeException {
-    public PermissionDeniedException(String message) {
-        super(message);
-    }
-}
