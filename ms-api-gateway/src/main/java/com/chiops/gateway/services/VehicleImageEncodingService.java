2cdb8c8c77b1c34d97efba19f3dea1f9c0a3ed04
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 22:03:12 -0600

    Delete ms-api-gateway directory

--- ms-api-gateway/src/main/java/com/chiops/gateway/services/VehicleImageEncodingService.java
+++
@@ -1,8 +0,0 @@
-package com.chiops.gateway.services;
-
-import com.chiops.gateway.libs.dtos.VehicleDTO;
-import io.micronaut.http.multipart.CompletedFileUpload;
-
-public interface VehicleImageEncodingService {
-    VehicleDTO createVehicleWithImageEncoded(VehicleDTO vehicle, CompletedFileUpload imageFile);
-}
