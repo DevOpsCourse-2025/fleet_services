98cf6de9b6470c7b2df49e5870468a3876a46419
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 22:06:22 -0600

    Delete ms-route-service directory

--- ms-route-service/src/main/java/com/chiops/route/libs/exceptions/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.route.libs.exceptions.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}
--- ms-route-service/src/main/java/com/chiops/route/libs/exceptions/exception/PermissionDeniedException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.route.libs.exceptions.exception;
-
-public class PermissionDeniedException extends RuntimeException {
-    public PermissionDeniedException(String message) {
-        super(message);
-    }
-}
