37bf3f1a0063aecf6eec1c4da47de6ac5127957f
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 21:24:04 -0600

    Update DriverController.java

--- ms-driver-service/src/main/java/com/chiops/driver/controllers/DriverController.java
+++ ms-driver-service/src/main/java/com/chiops/driver/controllers/DriverController.java
@@ -41,6 +41,18 @@ public class DriverController {
         }
     }
 
+    @Delete("/delete/{curp}")
+    public void deleteDriver(@PathVariable String curp) {
+        try {
+            driverService.deleteDriver(curp);
+        } catch (BadRequestException e) {
+            throw new BadRequestException("Bad request while trying to delete the driver with CURP " + curp + ": " + e.getMessage());
+        } catch (InternalServerException e) {
+            throw new InternalServerException("Internal server error while trying to delete the driver with CURP " + curp + ": " + e.getMessage());
+        }
+    }
+
+
 
     @Get("/get/{curp}")
     public DriverDTO getDriverByCurp(@PathVariable String curp) {
