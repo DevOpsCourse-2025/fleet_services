fa2521df6a205cb7600252b481d3f2a39ba5bd1b
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 21:19:13 -0600

    Delete ms-auth-service directory

--- ms-auth-service/src/main/java/com/chiops/auth/libs/exceptions/exception/MethodNotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.auth.libs.exceptions.exception;
-
-public class MethodNotAllowedException extends RuntimeException {
-    public MethodNotAllowedException(String message) {
-        super(message);
-    }
-}
--- ms-auth-service/src/main/java/com/chiops/auth/libs/exceptions/exception/PreconditionFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.auth.libs.exceptions.exception;
-
-public class PreconditionFailedException extends RuntimeException {
-    public PreconditionFailedException(String message) {
-        super(message);
-    }
-}
