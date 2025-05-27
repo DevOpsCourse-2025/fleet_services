727dde3cc389c676588da2e68ac35c67a8ee9639
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 21:18:58 -0600

    Delete ms-administrator-service directory

--- ms-administrator-service/src/main/java/com/chiops/administrator/libs/exceptions/exception/PreconditionFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.administrator.libs.exceptions.exception;
-
-public class PreconditionFailedException extends RuntimeException {
-    public PreconditionFailedException(String message) {
-        super(message);
-    }
-}
