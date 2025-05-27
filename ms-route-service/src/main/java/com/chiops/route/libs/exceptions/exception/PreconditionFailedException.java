e3c3db925e6c3243ab9398591393ddf661402ba3
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 21:19:42 -0600

    Delete ms-route-service directory

--- ms-route-service/src/main/java/com/chiops/route/libs/exceptions/exception/PreconditionFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.route.libs.exceptions.exception;
-
-public class PreconditionFailedException extends RuntimeException {
-    public PreconditionFailedException(String message) {
-        super(message);
-    }
-}
--- ms-route-service/src/main/java/com/chiops/route/libs/exceptions/exception/UnsupportedMediaTypeException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.route.libs.exceptions.exception;
-
-public class UnsupportedMediaTypeException extends RuntimeException {
-    public UnsupportedMediaTypeException(String message) {
-        super(message);
-    }
-}
