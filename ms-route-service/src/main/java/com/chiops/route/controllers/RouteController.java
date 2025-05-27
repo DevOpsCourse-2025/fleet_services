12b7e116b451968e4402c45beaff247be47cdad3
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 21:45:42 -0600

    Update RouteController.java

--- ms-route-service/src/main/java/com/chiops/route/controllers/RouteController.java
+++ ms-route-service/src/main/java/com/chiops/route/controllers/RouteController.java
@@ -36,7 +36,18 @@ public class RouteController {
             throw new InternalServerException("Error interno al crear la ruta: " + e.getMessage());
         }
     }
-
+    
+    @Put("/update")
+    public RouteDTO updateRoute(@Body RouteDTO route) {
+        try {
+            return routeService.updateRoute(route);
+        } catch (BadRequestException e) {
+            throw new BadRequestException("Error de solicitud al actualizar la ruta: " + e.getMessage());
+        } catch (InternalServerException e) {
+            throw new InternalServerException("Error interno al actualizar la ruta: " + e.getMessage());
+        }
+    }
+    
     @Get("/getall")
     public Iterable<RouteDTO> getAllRoutes() {
         try {
