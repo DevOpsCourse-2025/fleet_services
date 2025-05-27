88270d9dce4ac51b4717684971caa6897cfb1109
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 21:48:24 -0600

    feat: ✨ add  delete of vehicle

--- ms-route-service/src/main/java/com/chiops/route/controllers/RouteController.java
+++ ms-route-service/src/main/java/com/chiops/route/controllers/RouteController.java
@@ -47,6 +47,17 @@ public class RouteController {
             throw new InternalServerException("Error interno al actualizar la ruta: " + e.getMessage());
         }
     }
+
+    @Delete("/delete/{vin}")
+    public RouteDTO deleteRoute(@PathVariable String vin) {
+        try {
+            return routeService.deleteRoute(vin);
+        } catch (BadRequestException e) {
+            throw new BadRequestException("Error de solicitud al eliminar la ruta: " + e.getMessage());
+        } catch (InternalServerException e) {
+            throw new InternalServerException("Error interno al eliminar la ruta: " + e.getMessage());
+        }
+    }
     
     @Get("/getall")
     public Iterable<RouteDTO> getAllRoutes() {
