1c662f5f397aa103a82a4b16f0bbb0b38d0fcc63
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 22:47:11 -0600

    Delete ms-administrator-service directory

--- ms-administrator-service/src/main/java/com/chiops/administrator/libs/exceptions/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.administrator.libs.exceptions.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}
--- ms-administrator-service/src/main/java/com/chiops/administrator/libs/exceptions/exception/PermissionDeniedException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.administrator.libs.exceptions.exception;
-
-public class PermissionDeniedException extends RuntimeException {
-    public PermissionDeniedException(String message) {
-        super(message);
-    }
-}
