068fe93b5adb177dcdc8f7070f7cacff3a099d0b
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 22:03:19 -0600

    Delete ms-auth-service directory

--- ms-auth-service/README.md
+++
@@ -1,1 +0,0 @@
-# MS Auth Service
--- ms-auth-service/src/main/java/com/chiops/auth/libs/exceptions/exception/NotAuthenticatedException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.auth.libs.exceptions.exception;
-
-public class NotAuthenticatedException extends RuntimeException {
-    public NotAuthenticatedException(String message) {
-        super(message);
-    }
-}
