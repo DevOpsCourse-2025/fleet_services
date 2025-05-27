bce681a0d36de5a0ee3d97671e8355268b830cf2
FernandoVS11 <97609743+FernandoVS11@users.noreply.github.com>
2025-05-26 21:19:49 -0600

    Delete ms-vehicle-service directory

--- ms-vehicle-service/src/main/java/com/chiops/vehicle/libs/exceptions/exception/UnsupportedMediaTypeException.java
+++
@@ -1,7 +0,0 @@
-package com.chiops.vehicle.libs.exceptions.exception;
-
-public class UnsupportedMediaTypeException extends RuntimeException {
-    public UnsupportedMediaTypeException(String message) {
-        super(message);
-    }
-}
